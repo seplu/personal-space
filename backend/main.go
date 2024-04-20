4e2aca1cc3996a0878109083fd308c9fd12be99a
Sebastian Pludowski <sepludowski@gmail.com>
2024-04-20 13:58:56 +0200

    Routes - signup

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"seplu.pl/personal-space/db"
+	"seplu.pl/personal-space/routes"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
+	routes.RegisterRoutes(server)
 	err := server.Run(":8080")
 	if err != nil {
 		return
