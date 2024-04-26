2372c02f610f6607640d28b203097102d3328f35
Sebastian Pludowski <sepludowski@gmail.com>
2024-04-26 21:14:51 +0200

    Login functionality

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -4,4 +4,5 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
 	server.POST("/api/v1/signup", signup)
+	server.POST("/api/v1/login", login)
 }
--- backend/routes/users.go
+++ backend/routes/users.go
@@ -20,3 +20,18 @@ func signup(context *gin.Context) {
 	}
 	context.JSON(http.StatusCreated, gin.H{"message": "User created!", "user": user})
 }
+
+func login(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+	err = user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid credentials."})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Logged in!"})
+}
