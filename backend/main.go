88153c61074fbefeb1596f6da4a05bf6128de5fa
Sebastian Pludowski <sepludowski@gmail.com>
2024-05-06 21:38:33 +0200

    Use ENV to define PORT

--- backend/main.go
+++ backend/main.go
@@ -4,13 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"seplu.pl/personal-space/db"
 	"seplu.pl/personal-space/routes"
+	"seplu.pl/personal-space/utils"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
 	routes.RegisterRoutes(server)
-	err := server.Run(":8080")
+	port := utils.GetEnv("PORT", "8080")
+	err := server.Run(":" + port)
 	if err != nil {
 		return
 	}
