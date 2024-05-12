90cb5db437c20c250b0994a762e1735ca0f046cb
Sebastian Pludowski <sepludowski@gmail.com>
2024-05-12 19:50:46 +0200

    Verify Token and Auth

--- backend/utils/jwt.go
+++ backend/utils/jwt.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"errors"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"time"
 )
@@ -17,21 +18,42 @@ func GenerateToken(email string, userId int64) (string, error) {
 	return token.SignedString([]byte(secretKey))
 }
 
-func VerifyToken(token string) error {
+func VerifyToken(token string) (int64, error) {
 	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		_, ok := token.Method.(*jwt.SigningMethodHMAC)
 
 		if !ok {
-			return nil, errors.New("Unexpected signing method.")
+			return nil, errors.New("unexpected signing method")
 		}
 		return []byte(secretKey), nil
 	})
 	if err != nil {
-		return errors.New("Could not parse token.")
+		return 0, errors.New("could not parse token")
 	}
 	tokenIsValid := parsedToken.Valid
 	if !tokenIsValid {
 		return errors.New("Invalid token.")
+		return 0, errors.New("invalid token")
 	}
-	return nil
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, errors.New("could not parse token claims")
+	}
+	userId := int64(claims["userId"].(float64))
+	return userId, nil
+}
+
+func Auth(context *gin.Context) {
+	token := context.GetHeader("Authorization")
+	if token == "" {
+		context.AbortWithStatusJSON(401, gin.H{"message": "Not authorized."})
+		return
+	}
+	uID, err := VerifyToken(token)
+	if err != nil {
+		context.AbortWithStatusJSON(401, gin.H{"message": "Not authorized."})
+		return
+	}
+	context.Set("userId", uID)
+	context.Next()
 }
