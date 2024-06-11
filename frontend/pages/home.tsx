2774d7f59a2e94b2c50945d07b86942ad48f0973
Sebastian Pludowski <sepludowski@gmail.com>
2024-06-11 17:58:40 +0200

    Login page

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -1,12 +1,64 @@
 // @ts-ignore
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
-export default function About() {
+export default function Home() {
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const navigate = useNavigate();
+
+    function login(event) {
+        event.preventDefault();
+        fetch('/api/v1/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                Username: username,
+                Password: password,
+            }),
+        }).then((response) => response.json()).then((data) => {
+            if (data.message === "Logged in!") {
+                localStorage.setItem("ps_token", data.token);
+                setUsername("");
+                setPassword("");
+                navigate("/dashboard")
+            } else {
+                alert(data.message)
+            }
+        })
+    }
+
     return (
-        <div>
-            <h1>Home</h1>
-            <Link to="/about">About</Link>
+        <div className="center-div">
+            <div className="login-div">
+                <div className="logo">Personal Space</div>
+                <form onSubmit={login} className="login-form">
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        defaultValue="username"
+                        className="text-input"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        defaultValue="password"
+                        className="text-input"
+                        value={password}
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button type="submit">LOGIN</button>
+                    <p className="message">&copy; 2024 - <a href="https://github.com/seplu/personal-space/releases">ver: 0.1.0</a></p>
+                </form>
+            </div>
         </div>
     );
 }
