6404ebf431e7d5cc68bc1d4c8de6e5f89ea6d8ad
Sebastian Pludowski <sepludowski@gmail.com>
2024-06-11 17:59:11 +0200

    Login page

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -1,6 +1,6 @@
 // @ts-ignore
 import React from "react";
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 export default function Home() {
     const [username, setUsername] = React.useState("");
