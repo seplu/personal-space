0a804b9a55f6705fbef0dcdca2201c0ae93507a3
Sebastian Pludowski <sepludowski@gmail.com>
2024-08-11 14:08:37 +0200

    Navbar part I

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,10 +1,55 @@
 import React from 'react'
+import {
+    IoCarSportOutline,
+    IoCashOutline,
+    IoHomeOutline,
+    IoLibraryOutline,
+    IoPulseOutline,
+    IoRibbonOutline
+} from "react-icons/io5";
 
 function Navbar() {
     return (
-        <div>
-            <h2>Navbar</h2>
-        </div>
+        <nav className="navigation navigation--inline">
+            <ul>
+                <li>
+                    <a href="#">
+                        <IoHomeOutline className="navigation-icon"/>
+                        <span className="invisible">Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <IoCarSportOutline className="navigation-icon"/>
+                        <span className="invisible">Car</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <IoCashOutline className="navigation-icon"/>
+                        <span className="invisible">Finance</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <IoPulseOutline className="navigation-icon"/>
+                        <span className="invisible">Health</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <IoRibbonOutline className="navigation-icon"/>
+                        <span className="invisible">Habits tracker</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <IoLibraryOutline className="navigation-icon"/>
+                        <span className="invisible">Collection</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
