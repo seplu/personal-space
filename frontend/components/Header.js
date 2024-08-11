011e59c98236df22241d9c8eeecbdb15ed2191fa
Sebastian Pludowski <sepludowski@gmail.com>
2024-08-11 14:08:58 +0200

    Header part II

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {IoFingerPrint} from "react-icons/io5";
 
 function Header() {
     return (
         <div className="header">
             <div className="logo">Logo</div>
             <div className="right-section">
-                <div className="profile-icon">Profile</div>
+                <div className="profile-icon">
+                    <IoFingerPrint className="navigation-icon"/>
+                    <span>
+                        Profile
+                    </span>
+                </div>
             </div>
         </div>
     );
