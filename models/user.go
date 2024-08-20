97aa4c27f822ef53ae8f2c530028daf7454d13dd
Sebastian Pludowski <sepludowski@gmail.com>
2024-08-20 23:57:45 +0200

    [BE] Fix password validation

--- models/user.go
+++ models/user.go
@@ -10,7 +10,7 @@ import (
 type User struct {
 	ID       int64  `json:"id"`
 	Username string `json:"username" binding:"required"`
-	Password string `json:"-" binding:"required"`
+	Password string `json:"password" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 }
 
