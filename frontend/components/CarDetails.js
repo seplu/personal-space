da0f50716fa1c0ba8affd29e71c67f477985ce8d
Sebastian Pludowski <sepludowski@gmail.com>
2024-09-24 23:36:54 +0200

    [FE] split formData

--- frontend/components/CarDetails.js
+++ frontend/components/CarDetails.js
@@ -8,15 +8,13 @@ const CarDetails = () => {
     const formRef = useRef(null);
     const { id } = useParams();
     const [carDetails, setCarDetails] = useState(null);
-    const [formData, setFormData] = useState({
-        DateTime: "",
-        Mileage: "",
-        TypeOfFuel: "",
-        UnitPrice: "",
-        TotalPrice: "",
-        LitersKwh: "",
-        Description: ""
-    });
+    const [DateTime, setDateTime] = useState("");
+    const [Mileage, setMileage] = useState("");
+    const [TypeOfFuel, setTypeOfFuel] = useState("");
+    const [UnitPrice, setUnitPrice] = useState("");
+    const [TotalPrice, setTotalPrice] = useState("");
+    const [LitersKwh, setLitersKwh] = useState("");
+    const [Description, setDescription] = useState("");
     const [errMsg, setErrMsg] = useState("");
 
     useEffect(() => {
@@ -35,14 +33,6 @@ const CarDetails = () => {
             }
         })();
     }, [id]);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
 
     const toggleForm = () => {
         setShowForm(!showForm);
@@ -66,37 +56,36 @@ const CarDetails = () => {
     }, [showForm]);
 
     const clearForm = () => {
-        setFormData({
-            DateTime: "",
-            Mileage: "",
-            TypeOfFuel: "",
-            UnitPrice: "",
-            TotalPrice: "",
-            LitersKwh: "",
-            Description: ""
-        });
+        setDateTime("");
+        setMileage("");
+        setTypeOfFuel("");
+        setUnitPrice("");
+        setTotalPrice("");
+        setLitersKwh("");
+        setDescription("");
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post(`/car/${id}/details`, formData, {
+            const response = await axios.post(`/car/${id}/details`,
+                JSON.stringify({
+                    DateTime: DateTime,
+                    Mileage: Mileage,
+                    TypeOfFuel: TypeOfFuel,
+                    UnitPrice: UnitPrice,
+                    TotalPrice: TotalPrice,
+                    LitersKwh: LitersKwh,
+                    Description: Description
+                }), {
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 withCredentials: true
             });
             const result = await response.data;
-            setCarDetails(prevState => [...prevState, result]);
-            setFormData({
-                DateTime: "",
-                Mileage: "",
-                TypeOfFuel: "",
-                UnitPrice: "",
-                TotalPrice: "",
-                LitersKwh: "",
-                Description: ""
-            });
+            console.log("Car details submitted successfully!", result);
+            clearForm();
         } catch (error) {
             console.error("There was an error submitting the car details!", error);
             setErrMsg("Error submitting car details");
@@ -113,49 +102,51 @@ const CarDetails = () => {
                 <form onSubmit={handleSubmit}>
                     <div onClick={toggleForm} className="close-form-icon"><IoClose size={36}/></div>
                     <div className="input-div first">
-                        <input id="datetime" className="input" type="date" placeholder=" " name="Date"/>
+                        <input id="datetime" className="input" type="date" placeholder=" " name="Date"
+                               value={DateTime}
+                               onChange={(e) => setDateTime(e.target.value)}/>
                         <div className="space"></div>
                         <label htmlFor="datetime" className="placeholder">Date</label>
                     </div>
                     <div className="input-div other">
                         <input id="mileage" className="input" type="text" placeholder=" " name="Mileage"
-                               value={formData.Mileage}
-                               onChange={handleChange}/>
+                               value={Mileage}
+                               onChange={(e)=> setMileage(e.target.value)}/>
                         <div className="space"></div>
                         <label htmlFor="mileage" className="placeholder">Mileage</label>
                     </div>
                     <div className="input-div other">
                         <input id="typeoffuel" className="input" type="text" placeholder=" " name="TypeOfFuel"
-                               value={formData.TypeOfFuel}
-                               onChange={handleChange}/>
+                               value={TypeOfFuel}
+                               onChange={(e)=> setTypeOfFuel(e.target.value)}/>
                         <div className="space"></div>
                         <label htmlFor="typeoffuel" className="placeholder">Type of Fuel</label>
                     </div>
                     <div className="input-div other">
                         <input id="unitprice" className="input" type="text" placeholder=" " name="UnitPrice"
-                               value={formData.UnitPrice}
-                               onChange={handleChange}/>
+                               value={UnitPrice}
+                               onChange={(e)=> setUnitPrice(e.target.value)}/>
                         <div className="space"></div>
                         <label htmlFor="unitprice" className="placeholder">Unit price</label>
                     </div>
                     <div className="input-div other">
                         <input id="totalprice" className="input" type="text" placeholder=" " name="TotalPrice"
-                               value={formData.TotalPrice}
-                               onChange={handleChange}/>
+                               value={TotalPrice}
+                               onChange={(e)=> setTotalPrice(e.target.value)}/>
                         <div className="space"></div>
                         <label htmlFor="totalprice" className="placeholder">Total price</label>
                     </div>
                     <div className="input-div other">
                         <input id="literskwh" className="input" type="text" placeholder=" " name="LitersKwh"
-                               value={formData.LitersKwh}
-                               onChange={handleChange}/>
+                               value={LitersKwh}
+                               onChange={(e)=> setLitersKwh(e.target.value)}/>
                         <div className="space"></div>
                         <label htmlFor="literskwh" className="placeholder">Liters/Kwh</label>
                     </div>
                     <div className="input-div other">
                         <input id="description" className="input" type="text" placeholder=" " name="Description"
-                               value={formData.Description}
-                               onChange={handleChange}/>
+                               value={Description}
+                               onChange={(e)=> setDescription(e.target.id)}/>
                         <div className="space"></div>
                         <label htmlFor="description" className="placeholder">Description</label>
                     </div>
